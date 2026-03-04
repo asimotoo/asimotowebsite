df3c1d98ce465115fbe041b79800e95d00a59049
asimotoo <asimoto0671@gmail.com>
2026-03-04 15:11:12 +0300

    Fix Vercel API routing using catch-all endpoint

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "drizzle-kit";
+import * as dotenv from "dotenv";
 
+dotenv.config({ path: "./.env.local" });
 export default defineConfig({
   out: "./migrations",
   schema: "./shared/schema.ts",
